3ba6ec7b68f409a85622814333538f2ec6ef142b
Jhelt <jonhelton405@gmail.com>
2023-06-05 00:35:42 -0400

    added models

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,30 @@
 const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thought: {
+      type: String,
+      required: true,
+      maxlength: 250
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    currentDate: {
+      type: Date,
+      default: Date.now()
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+)
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,34 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    thoughts: [thoughtSchema],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      }
+    ]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
